42007cb769765fbeeef0b4d85166586fa93bd675
Madhuri-1510 <112881665+Madhuri-1510@users.noreply.github.com>
2022-09-05 22:07:29 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,13 @@
+import java.*;
+public class Addition
+{
+public static void main(String [] args)
+{
+int a=5;
+int b=6;
+int sum=a+b;
+int sum1=a-b;
+System.out.println("Addition is"+ sum);
+System.out.println("Addition is"+ sum1);
+}
+}
