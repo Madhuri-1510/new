d66d0d108becbcfd31728c53bba66a7a833a7486
Madhuri-1510 <112881665+Madhuri-1510@users.noreply.github.com>
2022-09-05 22:10:30 +0530

    Add files via upload

---
+++ Madhuri.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Madhuri
+{
+public static void main(String [] args)
+{
+//System.out.println("Hello Madhuri");
+String name;
+Scanner mauu=new Scanner(System.in);
+name =mauu.nextLine();
+System.out.println(name);
+
+
+/*
+int a=10;
+int b=12;
+int c=a-b;
+System.out.println(c);
+*/
+}
+}
+
